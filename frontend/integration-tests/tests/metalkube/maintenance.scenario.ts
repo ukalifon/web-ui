2e0e48cf0210bd3a9e018c1a0fc6a34a84ad1d7e
Udi Kalifon <ukalifon@redhat.com>
2019-05-02 16:59:57 +0200

    Remove sleep, and parametrize the node name
    Using browser.wait(until ...) instead of sleep(4000). Also introduced
    a const with the name of the master node, so we don't repeat the string
    literal in so many places.

--- frontend/integration-tests/tests/metalkube/maintenance.scenario.ts
+++ frontend/integration-tests/tests/metalkube/maintenance.scenario.ts
@@ -4,7 +4,9 @@ import { appHost } from '../../protractor.conf';
 import * as crudView from '../../views/crud.view';
 import * as bareMetalHostView from '../../views/metalkube/baremetalhost.view';
 
-describe('Start and stop maintenance master-0', () => {
+const masterNode = 'openshift-master-0';
+
+describe(`Start and stop maintenance ${masterNode}`, () => {
   it('go to /k8s/all-namespaces/baremetalhosts', async() => {
     // FIXME: prefer navigating using the actual nav pane, to test that it's working and we're not just bypassing it
     await browser.get(`${appHost}/k8s/all-namespaces/baremetalhosts`);
@@ -13,27 +15,27 @@ describe('Start and stop maintenance master-0', () => {
     expect(crudView.createYAMLButton.getText()).toEqual('Add Host');
   });
 
-  it('find master-0 and check its status', async() => {
-    browser.wait(until.presenceOf(crudView.rowForName('openshift-master-0')));
-    expect(crudView.rowForName('openshift-master-0').isDisplayed()).toBe(true);
-    expect(bareMetalHostView.hostname('openshift-master-0').isDisplayed()).toBe(true);
-    const stat = await bareMetalHostView.status('openshift-master-0').getText();
+  it(`find ${masterNode} and check its status`, async() => {
+    browser.wait(until.presenceOf(crudView.rowForName(masterNode)));
+    expect(crudView.rowForName(masterNode).isDisplayed()).toBe(true);
+    expect(bareMetalHostView.hostname(masterNode).isDisplayed()).toBe(true);
+    const stat = await bareMetalHostView.status(masterNode).getText();
     expect(stat === 'Provisioned' || stat === 'Externally provisioned').toBe(true);
   });
 
-  it('start maintenance on master-0', async() => {
-    await crudView.selectOptionFromGear('openshift-master-0', 'Start Maintenance');
+  it(`start maintenance on ${masterNode}`, async() => {
+    await crudView.selectOptionFromGear(masterNode, 'Start Maintenance');
     await bareMetalHostView.confirmAction();
-    browser.sleep(4000);
-    const stat = await bareMetalHostView.status('openshift-master-0').getText();
+    browser.wait(until.textToBePresentInElement(bareMetalHostView.status(masterNode), 'Starting maintenance'), 5000);
+    const stat = await bareMetalHostView.status(masterNode).getText();
     expect(stat).toEqual('Starting maintenance');
   });
 
-  it('stop maintenance on master-0', async() => {
-    await crudView.selectOptionFromGear('openshift-master-0', 'Stop Maintenance');
+  it(`stop maintenance on ${masterNode}`, async() => {
+    await crudView.selectOptionFromGear(masterNode, 'Stop Maintenance');
     await bareMetalHostView.confirmAction();
-    browser.sleep(4000);
-    const stat = await bareMetalHostView.status('openshift-master-0').getText();
+    browser.wait(until.textToBePresentInElement(bareMetalHostView.status(masterNode), 'rovisioned'), 5000);
+    const stat = await bareMetalHostView.status(masterNode).getText();
     expect(stat === 'Provisioned' || stat === 'Externally provisioned').toBe(true);
   });
 });
